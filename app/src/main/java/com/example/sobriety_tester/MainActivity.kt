ad3fb2627e9d32f641e170dded319796b3d7f5ca
Julian Blaschke <julianblaschke@icloud.com>
2025-06-18 16:24:12 +0200

    project initialization

---
+++ app/src/main/java/com/example/sobriety_tester/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.sobriety_tester
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sobriety_tester.ui.theme.SobrietytesterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SobrietytesterTheme {
+                Text("Sobriety Tester")
+            }
+        }
+    }
+}
